f3907fae28dbadfb3152beda9a08af6900a798df
BrianCaetano <brian-caetano@hotmail.com>
2021-07-08 19:51:08 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-  const selected = document.querySelectorAll('.selected');
-  const color = document.querySelectorAll('.color');
+const selected = document.querySelectorAll('.selected');
+const color = document.querySelectorAll('.color');
+const allPixels = document.querySelectorAll('#pixel-board .pixel-row .pixel');
 
+function changeClassSelected() {
   color.forEach(item => {
-    item.addEventListener('click', event => {
+    item.addEventListener('click', () => {
       for (let i = 0; i < color.length; i += 1) {
         color[i].classList.remove('selected');
       }
       item.classList.add('selected');
     });
+  });
+} changeClassSelected()
+
+function getSelectedColor() {
+  allPixels.forEach((index) => {
+    index.addEventListener('click', (event) => {
+      const pixel = event.target.style;
+      pixel.backgroundColor = window.getComputedStyle(document.querySelector('.selected')).getPropertyValue('background-color');
+    });
   })
+} getSelectedColor()
+
